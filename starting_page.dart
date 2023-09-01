8c78d3d2e480dc16ec74163347375b26e9f9882d
Hee Joon Yi <huijunyi0212@naver.com>
2023-09-02 01:08:49 +0900

    .

--- starting_page.dart
+++ starting_page.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class StartingPage extends StatelessWidget {
+enum Dorms { aw, am, bw, bm }
+
+class StartingPage extends StatefulWidget {
   const StartingPage({super.key});
+
+  @override
+  State<StartingPage> createState() => _StartingPageState();
+}
+
+class _StartingPageState extends State<StartingPage> {
+  final dormsSelected = [true, true, true, true];
+
+  void selectDorm(int index) {
+    setState(() {
+      for (int i = 0; i < 4; i++) {
+        if (i == index) {
+          dormsSelected[i] = true;
+        } else {
+          dormsSelected[i] = false;
+        }
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +52,7 @@ class StartingPage extends StatelessWidget {
                   letterSpacing: -2,
                   height: 0.8,
                   fontSize: 43,
-                  color: Color(0xff2E3784),
+                  color: Color(0xff1B3D71),
                 ),
                 textAlign: TextAlign.left,
               ),
@@ -49,37 +70,49 @@ class StartingPage extends StatelessWidget {
                     Container(
                       margin: const EdgeInsets.all(10),
                       decoration: BoxDecoration(
-                        color: const Color(0xff2E3784),
+                        color: dormsSelected[0] == true
+                            ? const Color(0xff2E3784)
+                            : const Color(0xffA0A2BA),
                         borderRadius: BorderRadius.circular(30),
                       ),
                       child: SizedBox(
                         width: 150,
                         height: 150,
-                        child: Center(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: const [
-                              Text(
-                                "A",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 58,
-                                  color: Colors.white,
+                        child: TextButton(
+                          style: TextButton.styleFrom(
+                            splashFactory: NoSplash.splashFactory,
+                            surfaceTintColor: Colors.transparent,
+                            backgroundColor: Colors.transparent,
+                          ),
+                          onPressed: () => {
+                            selectDorm(0),
+                          },
+                          child: const Center(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "A",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 58,
+                                    color: Colors.white,
+                                  ),
                                 ),
-                              ),
-                              Text(
-                                "Women",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 18,
-                                  color: Colors.white,
+                                Text(
+                                  "Women",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                    color: Colors.white,
+                                  ),
                                 ),
-                              ),
-                              SizedBox(
-                                width: 10,
-                                height: 20,
-                              ),
-                            ],
+                                SizedBox(
+                                  width: 10,
+                                  height: 20,
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       ),
@@ -87,37 +120,49 @@ class StartingPage extends StatelessWidget {
                     Container(
                       margin: const EdgeInsets.all(10),
                       decoration: BoxDecoration(
-                        color: const Color(0xff2E3784),
+                        color: dormsSelected[1] == true
+                            ? const Color(0xff2E3784)
+                            : const Color(0xffA0A2BA),
                         borderRadius: BorderRadius.circular(30),
                       ),
                       child: SizedBox(
                         width: 150,
                         height: 150,
-                        child: Center(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: const [
-                              Text(
-                                "A",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 58,
-                                  color: Colors.white,
+                        child: TextButton(
+                          style: TextButton.styleFrom(
+                            splashFactory: NoSplash.splashFactory,
+                            surfaceTintColor: Colors.transparent,
+                            backgroundColor: Colors.transparent,
+                          ),
+                          onPressed: () => {
+                            selectDorm(1),
+                          },
+                          child: const Center(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "A",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 58,
+                                    color: Colors.white,
+                                  ),
                                 ),
-                              ),
-                              Text(
-                                "Men",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 18,
-                                  color: Colors.white,
+                                Text(
+                                  "Men",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                    color: Colors.white,
+                                  ),
                                 ),
-                              ),
-                              SizedBox(
-                                width: 10,
-                                height: 20,
-                              ),
-                            ],
+                                SizedBox(
+                                  width: 10,
+                                  height: 20,
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       ),
@@ -130,37 +175,49 @@ class StartingPage extends StatelessWidget {
                     Container(
                       margin: const EdgeInsets.all(10),
                       decoration: BoxDecoration(
-                        color: const Color(0xff2E3784),
+                        color: dormsSelected[2] == true
+                            ? const Color(0xff2E3784)
+                            : const Color(0xffA0A2BA),
                         borderRadius: BorderRadius.circular(30),
                       ),
                       child: SizedBox(
                         width: 150,
                         height: 150,
-                        child: Center(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: const [
-                              Text(
-                                "B",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 58,
-                                  color: Colors.white,
+                        child: TextButton(
+                          style: TextButton.styleFrom(
+                            splashFactory: NoSplash.splashFactory,
+                            surfaceTintColor: Colors.transparent,
+                            backgroundColor: Colors.transparent,
+                          ),
+                          onPressed: () => {
+                            selectDorm(2),
+                          },
+                          child: const Center(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "B",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 58,
+                                    color: Colors.white,
+                                  ),
                                 ),
-                              ),
-                              Text(
-                                "Women",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 18,
-                                  color: Colors.white,
+                                Text(
+                                  "Women",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                    color: Colors.white,
+                                  ),
                                 ),
-                              ),
-                              SizedBox(
-                                width: 10,
-                                height: 20,
-                              ),
-                            ],
+                                SizedBox(
+                                  width: 10,
+                                  height: 20,
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       ),
@@ -168,37 +225,49 @@ class StartingPage extends StatelessWidget {
                     Container(
                       margin: const EdgeInsets.all(10),
                       decoration: BoxDecoration(
-                        color: const Color(0xff2E3784),
+                        color: dormsSelected[3] == true
+                            ? const Color(0xff2E3784)
+                            : const Color(0xffA0A2BA),
                         borderRadius: BorderRadius.circular(30),
                       ),
                       child: SizedBox(
                         width: 150,
                         height: 150,
-                        child: Center(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: const [
-                              Text(
-                                "B",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 58,
-                                  color: Colors.white,
+                        child: TextButton(
+                          style: TextButton.styleFrom(
+                            splashFactory: NoSplash.splashFactory,
+                            surfaceTintColor: Colors.transparent,
+                            backgroundColor: Colors.transparent,
+                          ),
+                          onPressed: () => {
+                            selectDorm(3),
+                          },
+                          child: const Center(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "B",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 58,
+                                    color: Colors.white,
+                                  ),
                                 ),
-                              ),
-                              Text(
-                                "Men",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 18,
-                                  color: Colors.white,
+                                Text(
+                                  "Men",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                    color: Colors.white,
+                                  ),
                                 ),
-                              ),
-                              SizedBox(
-                                width: 10,
-                                height: 20,
-                              ),
-                            ],
+                                SizedBox(
+                                  width: 10,
+                                  height: 20,
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       ),
@@ -209,22 +278,27 @@ class StartingPage extends StatelessWidget {
                   width: 320,
                   height: 60,
                 ),
-                Container(
-                  decoration: BoxDecoration(
-                    color: const Color(0xff1B3D71),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: const SizedBox(
-                    width: 320,
-                    height: 50,
-                    child: Center(
+                SizedBox(
+                  width: 320,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: () => {
+                      print("hejwlew"),
+                    },
+                    style: ElevatedButton.styleFrom(
+                      elevation: 1,
+                      backgroundColor: const Color(0xff1B3D71),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: const Center(
                       child: Text(
                         "Next",
-                        textAlign: TextAlign.center,
                         style: TextStyle(
-                          fontWeight: FontWeight.bold,
                           color: Colors.white,
                           fontSize: 27,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
                     ),
