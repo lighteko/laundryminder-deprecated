5f2d9dfca7d1bce102839a50f210e1c198822310
Hee Joon Yi <huijunyi0212@naver.com>
2023-09-08 00:06:06 +0900

    .

---
+++ services/api_service.dart
@@ -0,0 +1,7 @@
+class ApiService {
+  final String baseUrl =
+      "https://port-0-laundryminder-fastapi-1xxfe2blm66cw9t.sel5.cloudtype.app";
+  final String machines = "machines";
+
+  void getMachines() {}
+}
